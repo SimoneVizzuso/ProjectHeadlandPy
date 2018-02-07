9ee86277e9060ae46a92d3124f17ea7fa5c48cef
simonevizzuso <simonevizzuso@outlook.com>
2018-02-07 11:31:56 +0100

    Changed rock with stick Added scroll class with life and damage scroll

--- adventure/items.py
+++ adventure/items.py
@@ -19,10 +19,19 @@ class Weapon(Item):
         return '{}\n=====\n{}\nDamage: {}'.format(self.name, self.description, self.damage)
 
 
-class Rock(Weapon):
+class Scroll(Item):
+    def __init__(self, name, description, effect):
+        self.effect = effect
+        super().__init__(name, description)
+
+    def __str__(self):
+        return '{}\n=====\n{}'.format(self.name, self.description)
+
+
+class Stick(Weapon):
     def __init__(self):
-        super().__init__(name='Rock',
-                         description='A rock that you found in your bag',
+        super().__init__(name='Stick',
+                         description='A wooden stick. It is a weak weapon to fight',
                          damage=5)
 
 
@@ -39,3 +48,19 @@ class OldMap(Item):
                          description='The exit is north but you must first explore the dungeon\nTo the west you could '
                                      'find something very powerful\nYou will have to go east, but beware of the '
                                      'danger')
+
+
+class LifeScroll(Scroll):
+    def __init__(self):
+        super().__init__(name='Life Scroll',
+                         description='A very strange scroll. It is written in an ancient language\nbut you seem able'
+                                     'to read it',
+                         effect=100)
+
+
+class DamageScroll(Scroll):
+    def __init__(self):
+        super().__init__(name='Damage Scroll',
+                         description='A red scroll with a drop of blood on the seal.\nIt seems to shine through '
+                                     'wickedness',
+                         effect=50)
